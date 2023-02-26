429b65d94a14ef19ad26e9a8df222559454b3c9f
nomyfan <Nomyfan@live.com>
2023-02-26 23:12:17 +0800

    tabs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -40,7 +40,7 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
-          includeCurrentVersion: process.env.NODE_ENV === "development",
+          // includeCurrentVersion: process.env.NODE_ENV === "development",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
